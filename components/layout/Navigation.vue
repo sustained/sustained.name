f029ba9667db848212065bc3673c1d1df087e4fa
sustained <sustained.dissonance@gmail.com>
2019-06-26 18:15:24 +0100

    Add logo and nav components.

---
+++ components/layout/Navigation.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav>
+    <ul>
+      <li>
+        <nuxt-link class="router-link" to="/">Home</nuxt-link>
+      </li>
+
+      <li>
+        <nuxt-link class="router-link" to="/blog">Blog</nuxt-link>
+      </li>
+
+      <li>
+        <nuxt-link class="router-link" to="/projects">Projects</nuxt-link>
+      </li>
+
+      <li>
+        <nuxt-link class="router-link" to="/portfolio">Portfolio</nuxt-link>
+      </li>
+
+      <li>
+        <nuxt-link class="router-link" to="/resume">Resume</nuxt-link>
+      </li>
+    </ul>
+  </nav>
+</template>
