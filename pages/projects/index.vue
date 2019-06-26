bd3122f70db74f178f015bd7bebce989b5af3ccb
sustained <sustained.dissonance@gmail.com>
2019-06-26 18:12:55 +0100

    Add some coming soon pages.

---
+++ pages/projects/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="large-title">Projects</h1>
+
+      <h2 class="large-subtitle">Coming soon.</h2>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  layout: "projects"
+};
+</script>
---
+++ pages/resume/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="large-title">Resume</h1>
+
+      <h2 class="large-subtitle">Coming soon.</h2>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  layout: "resume"
+};
+</script>
