8202770bf4293c8792db7f8e1685be66f6fa7e6f
sustained <sustained.dissonance@gmail.com>
2019-06-26 21:04:39 +0100

    Log payload and params.

--- pages/blog/tags/_tag.vue
+++ pages/blog/tags/_tag.vue
@@ -1,3 +1,22 @@
 <template>
-  <div>specific tag?</div>
+  <div>
+    <h1>I am a tag</h1>
+
+    <h2>Params</h2>
+    <pre>{{ params }}</pre>
+
+    <h2>Payload</h2>
+    <pre>{{ payload }}</pre>
+  </div>
 </template>
+
+<script>
+export default {
+  asyncData({ params, error, payload }) {
+    return {
+      params,
+      payload
+    };
+  }
+};
+</script>
