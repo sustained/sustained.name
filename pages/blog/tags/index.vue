6d4553e16efd7ebc7e0d69e9c504d5fb63eede90
sustained <sustained.dissonance@gmail.com>
2019-06-27 01:54:42 +0100

    Make tags page line up with rest of pages.

--- pages/blog/tags/index.vue
+++ pages/blog/tags/index.vue
@@ -1,26 +1,26 @@
 <template>
-  <div>
-    <h1>Tags</h1>
+  <section class="container-fixed pt-4">
+    <header class="flex items-center px-4">
+      <h1 class="normal-title flex-1">Tag List</h1>
+    </header>
 
-    <pre>{{ tags }}</pre>
-  </div>
+    <main>
+      <div class="card">
+        <ul class="flex w-full flex-wrap items-center justify-center text-center">
+          <li v-for="(tag, index) in tags" :key="index" class="flex-1 p-6 min-w-1/3">
+            <nuxt-link :to="tag.path" class="link text-2xl">{{ tag.name}}</nuxt-link>
+          </li>
+        </ul>
+      </div>
+    </main>
+  </section>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      tags: []
-    };
-  },
-
-  async created() {
-    try {
-      const tags = await import("static/json/tags.json");
-
-      this.tags = tags.default;
-    } catch (e) {
-      console.error("Unable to load JSON. :(", e.message);
+  computed: {
+    tags() {
+      return this.$store.state.blog.tags;
     }
   },
 
