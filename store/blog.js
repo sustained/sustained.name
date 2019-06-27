0241cc81415cf919e6f17fa770582232cadbe272
sustained <sustained.dissonance@gmail.com>
2019-06-27 01:31:27 +0100

    Clean up store.

--- store/blog.js
+++ store/blog.js
@@ -1,62 +1,39 @@
-import { articles, categories, normalise } from "~/data/blog.js";
-
-function createArticles() {
-  return articles.map(article => {
-    return Object.assign({}, article, {
-      path: `/blog/${normalise(article.category)}/${normalise(article.title)}`
-    });
-  });
-}
-
-function createCategories() {
-  return categories.map(category => {
-    return Object.assign({}, category, {
-      path: `/blog/${normalise(category.name)}`
-    });
-  });
-}
-
 export const state = () => {
   return {
-    articles: createArticles(),
-    categories: createCategories()
+    tags: [],
+    articles: [],
+    categories: []
   };
 };
 
+export const mutations = {
+  setTags: (state, payload) => (state.tags = payload),
+  setArticles: (state, payload) => (state.articles = payload),
+  setCategories: (state, payload) => (state.categories = payload)
+};
+
 export const getters = {
-  tags(state) {
-    const tags = Object.values(state.articles).reduce((tags, article) => {
-      article.tags.map(tag => {
-        if (!tags[tag]) {
-          tags.push({
-            name: tag,
-            path: `/blog/tags/${normalise(tag)}`,
-            count: 1
-          });
-        } else {
-          tags[tag].count++;
-        }
-      });
-
-      return tags;
-    }, []);
-
-    // console.log("!!!", tags);
-
-    return tags;
-  },
-
-  articlesByTag(state) {
-    return tagName => {
-      return state.articles.filter(article => article.tags.includes(tagName));
+  articlesByTagName(state) {
+    return name => {
+      return state.articles.filter(article => article.tags.includes(name));
     };
   },
 
-  articlesByCategory(state) {
-    return categoryName => {
-      return state.articles.filter(
-        article => article.category === categoryName
-      );
+  articlesByCategorySlug(state) {
+    return slug => {
+      return state.articles.filter(article => article.categorySlug === slug);
+    };
+  },
+
+  getCategoryForSlug(state) {
+    return slug => {
+      return state.categories.find(category => category.slug === slug);
+    };
+  },
+
+  tagByTagSlug(state) {
+    return slug => {
+      return state.tags.find(tag => tag.slug === slug);
     };
   }
 };
