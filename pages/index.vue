f1fd46e30e94e5b72e4a605b48eb1962ddcdc30c
sustained <sustained.dissonance@gmail.com>
2019-07-04 17:58:39 +0100

    Accidentally pushing to the wrong branch FTL. (#11)
    * Add test blog articles.
    
    * Add a 404 page.
    
    * Update deps.
    
    + gray-matter
    
    * Add meta entries for categories.
    
    * Remove now unused blog data file.
    
    * Generate blog routes; generate blog JSON on build.
    
    * Ignore generated JSON.
    
    * Clean up store.
    
    * Add plugin to populate store from generated JSON.
    
    * Add blog lib.
    
    * Remove old highlighting imports + lang. registration.
    
    * Update component registration-related code.
    
    * Add excerpts; update component references.
    
    * Update views to reflect changes.
    
    * Account for when components is unset.
    
    * Move state to store.
    
    * Make tags page line up with rest of pages.
    
    * Remove unnecessary CSS.
    
    * Handle nonexistent tags/categories.
    
    * Adjust text sizing.
    
    * Add some blog-specific styling.
    
    * Add test image.
    
    * Adjust headings.
    
    * Add emojis, anchors, TOC.
    
    * Add markdown testing article.
    
    * Allow viewing either raw markdown or rendered html.
    
    * Add test image (static).
    
    * Use reduce technique instead of flat.
    
    * Enable no-ssr.
    
    * Remove no-ssr.
    
    * Register global components.
    
    * Fix pre-wrapping issue.
    
    * Add global code-block component.
    
    * Style adjustments.
    
    * Scroll to hash if present, closes #6.
    
    * Fix typo.
    
    * Error handling.
    
    * Emoji.
    
    * ESLint annoys me. >_>
    
    * Add requires-js feature.
    
    Hide elements when JS is disabled by using the `requires-js` class.
    
    * SSR stylesheet to fix styling issues when JS disabled.
    
    * Remove old logging.
    
    * Add lang attribute.
    
    * Delete all but one testing article; set up categories.
    
    * Accents are pretty cool guys who don't afraid of vowels.
    
    * Fix head stuff.
    
    * Adjust colours slightly.

--- pages/index.vue
+++ pages/index.vue
@@ -6,8 +6,8 @@
       <h2 class="large-subtitle">My (very WIP) personal website.</h2>
 
       <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button mr-4">Documentation</a>
-        <a href="https://github.com/sustained/sustained.name" target="_blank" class="button">GitHub</a>
+        <a href="https://nuxtjs.org/" target="_blank" rel="noopener" class="button mr-4">Documentation</a>
+        <a href="https://github.com/sustained/sustained.name" rel="noopener" target="_blank" class="button">GitHub</a>
       </div>
     </div>
   </section>
