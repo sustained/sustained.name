8e82d93e547c1fb1edb4fcadc577c6d120857e5b
sustained <sustained.dissonance@gmail.com>
2019-07-12 06:48:13 +0100

    Fix paths + styling.

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,32 +1,34 @@
 <template>
   <section class="container">
-    <div>
+    <div class="text-left">
       <h1 class="large-title">Projects</h1>
 
-      <ul>
+      <ul class="list-disc list-outside">
         <li>
-          <nuxt-link to="projects/ascension" class="text-green-600 text-xl my-8">Ascension</nuxt-link>
+          <nuxt-link to="/projects/ascension" class="text-green-600 text-xl my-8">Ascension</nuxt-link>
         </li>
 
         <li>
-          <nuxt-link to="projects/cello" class="text-green-600 text-xl my-8">Cello</nuxt-link>
+          <nuxt-link to="/projects/cello" class="text-green-600 text-xl my-8">Cello</nuxt-link>
         </li>
 
         <li>
-          <nuxt-link to="projects/fladdermus" class="text-green-600 text-xl my-8">Fladdermus</nuxt-link>
+          <nuxt-link to="/projects/fladdermus" class="text-green-600 text-xl my-8">Fladdermus</nuxt-link>
         </li>
 
         <li>
-          <nuxt-link to="projects/motion" class="text-green-600 text-xl my-8">Motion.js</nuxt-link>
+          <nuxt-link to="/projects/motion" class="text-green-600 text-xl my-8">Motion.js</nuxt-link>
         </li>
 
         <li>
-          <nuxt-link to="projects/sforzando" class="text-green-600 text-xl my-8">Sforzando</nuxt-link>
+          <nuxt-link to="/projects/sforzando" class="text-green-600 text-xl my-8">Sforzando</nuxt-link>
         </li>
 
         <li>
-          <nuxt-link to="projects/website" class="text-green-600 text-xl my-8">sustained.name</nuxt-link>
+          <nuxt-link to="/projects/website" class="text-green-600 text-xl my-8">sustained.name</nuxt-link>
         </li>
+
+        <li class="mt-4 list-none">More projects coming soon.</li>
       </ul>
     </div>
   </section>
