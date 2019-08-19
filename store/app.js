a992da33ed4d8319d82b09add5290417b940f409
sustained <sustained.dissonance@gmail.com>
2019-08-19 19:51:45 +0100

    Add project metadata to main vuex store.

--- store/app.js
+++ store/app.js
@@ -1,6 +1,8 @@
 export const state = () => {
   return {
-    isNavVisible: true
+    isNavVisible: true,
+
+    projects: {}
   };
 };
 
@@ -8,5 +10,21 @@ export const mutations = {
   setNavVisible: (state, payload) => (state.isNavVisible = payload),
 
   toggleNavVisible: (state, payload) =>
-    (state.isNavVisible = !state.isNavVisible)
+    (state.isNavVisible = !state.isNavVisible),
+
+  setProjects(state, payload) {
+    state.projects = payload;
+  }
+};
+
+export const actions = {
+  setProjects({ commit }, projects) {
+    commit("setProjects", projects);
+  }
+};
+
+export const getters = {
+  isValidProject(state) {
+    return projectName => !!state.projects[projectName];
+  }
 };
