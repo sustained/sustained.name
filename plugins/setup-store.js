53e4b6381bb2b230714c9d53eb601a9edf1d40a5
sustained <sustained.dissonance@gmail.com>
2019-08-20 07:38:28 +0100

    Remove old plugins.

--- plugins/setup-store.js
+++
@@ -1,9 +0,0 @@
-export default async ({ store }) => {
-  const tags = await import("static/json/tags.json");
-  const articles = await import("static/json/articles.json");
-  const categories = await import("static/json/categories.json");
-
-  store.commit("blog/setTags", tags.default);
-  store.commit("blog/setArticles", articles.default);
-  store.commit("blog/setCategories", categories.default);
-};
