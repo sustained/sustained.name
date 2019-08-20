3afb123ba3f05323ea281b972f54c5b16f2c2e53
sustained <sustained.dissonance@gmail.com>
2019-08-20 07:34:25 +0100

    Allow passing any amount of path components to be resolved.

--- library/file-utils.js
+++ library/file-utils.js
@@ -1,9 +1,10 @@
 import { promises } from "fs";
+import { resolve } from "path";
 const { readdir } = promises;
 
-export async function getDirectory(path) {
+export async function getDirectory(...pathComponents) {
   try {
-    const files = await readdir(path);
+    const files = await readdir(resolve(...pathComponents));
 
     return files;
   } catch (e) {
