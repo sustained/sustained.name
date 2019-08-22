5b6e0dc5cfee4481edafd94a4cdc814fd51f9200
sustained <sustained.dissonance@gmail.com>
2019-08-22 03:25:25 +0100

    Add ProjectInfo component.

--- pages/projects/_project.vue
+++ pages/projects/_project.vue
@@ -1,5 +1,6 @@
 <template>
   <section class="container-fixed pt-4">
+    <ProjectInfo v-bind="project.attributes" />
     <DynamicMarkdown v-bind="project" />
   </section>
 </template>
@@ -7,11 +8,12 @@
 <script>
 import { getMeta, setHead, asyncData } from "~/modules/dynamic-markdown-mixin";
 
+import ProjectInfo from "@/components/projects/ProjectInfo.vue";
+
 export default {
   layout: "projects",
-
+  components: { ProjectInfo },
   mixins: [getMeta, setHead],
-
   asyncData
 };
 </script>
