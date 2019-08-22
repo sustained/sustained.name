394d6456141f7094723aa52b32798c6cfdb8da01
sustained <sustained.dissonance@gmail.com>
2019-08-22 03:23:44 +0100

    Comments; add (custom) attributes + merge to data.

--- components/DynamicMarkdown.vue
+++ components/DynamicMarkdown.vue
@@ -2,8 +2,10 @@
 import Vue from "vue";
 
 export default {
-  // eslint-disable-next-line
   props: {
+    /*
+      The render function provided by frontmatter-markdown-loader.
+    */
     renderFunction: {
       type: String,
 
@@ -11,6 +13,10 @@ export default {
         return "return function render() {};";
       }
     },
+
+    /*
+      The static render functions provided by frontmatter-markdown-loader.
+    */
     staticRenderFunctions: {
       type: String,
 
@@ -18,13 +24,68 @@ export default {
         return "return [];";
       }
     },
+
+    /*
+      A list of components to load.
+    */
     componentList: {
       type: Array,
 
       default() {
         return [];
       }
+    },
+
+    /*
+      The frontmatter attributes which will be merge into data and
+      accessible via e.g. {{ attributeName }} in markdown files.
+    */
+    attributes: {
+      type: Object,
+
+      default() {
+        return {};
+      }
+    },
+
+    /*
+      Custom attributes to merge into the frontmatter attributes.
+
+      Example:
+
+        Vue component:
+
+          <DynamicMarkdown v-bind="entity" :custom-attributes="{ foo: 'bar' }" />
+
+        Markdown file:
+
+          {{ foo }}
+    */
+    customAttributes: {
+      type: Object,
+
+      default() {
+        return {};
+      }
     }
+  },
+
+  /*
+    Merge in YAML frontmatter attributes so they can be used in
+    the markdown if desired.
+  */
+  data() {
+    const data = {};
+
+    for (const [key, value] of Object.entries(this.attributes)) {
+      data[key] = value;
+    }
+
+    for (const [key, value] of Object.entries(this.customAttributes)) {
+      data[key] = value;
+    }
+
+    return data;
   },
 
   beforeDestroy() {
@@ -33,11 +94,18 @@ export default {
 
   created() {
     this.registerComponents();
-    this.renderer = new Function(this.renderFunction)();
+
+    this.renderer = new Function(this.renderFunction).bind()();
     this.$options.staticRenderFns = new Function(this.staticRenderFunctions)();
   },
 
   methods: {
+    /*
+      HACK: We define global components temporarily so that we can access a
+            component as <Foo /> in the markdown since there is no good  way
+            to dynamically set components so only alternative would be e.g.
+            <Component is="components.Foo" /> which is awful in its own right.
+    */
     registerComponents() {
       this.componentList.forEach(component => {
         const [name, path] = this.getComponentNameAndPath(component);
